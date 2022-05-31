a2911575e0291ae834dc4683a0e8710b8ddad237
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-05-31 06:48:51 +0300

    It will make reset button responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 let btnCalculate=document.getElementById("calculate");
+let btnReset=document.getElementById("reset");
 let billAmount=document.getElementById("bill");
 let parcentage=document.getElementById("parcentage");
 let tipAmount=document.getElementById("amount");
 let total=document.getElementById("total");
+let formEl=document.getElementById("form");
 let amount;
 let sum;
-
+//calculate button
 btnCalculate.addEventListener("click", ()=>{
     let bill=billAmount.value;
     let parcent=parcentage.value;
@@ -13,6 +15,16 @@ btnCalculate.addEventListener("click", ()=>{
     tipAmount.value=amount;
     sum=amount + parseFloat(bill);
     total.value=sum;
+   
+})
+//empty inputs
+
+// reset button
+btnReset.addEventListener("click" ,()=>{
+    billAmount.value="";
+    parcentage.value="";
+    tipAmount.value="";
+    total.value="";
 
 
 })
